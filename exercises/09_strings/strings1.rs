88263c39b901b2bc1330453979eb0f474cfd3273
Rafael Campos <racampos@gmail.com>
2024-03-16 13:58:29 -0600

    Progress up to 09-04

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -5,13 +5,14 @@
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    // Solution 1:
+    // String::from("blue")
+    // Solution 2:
+    "blue".to_string()
 }
